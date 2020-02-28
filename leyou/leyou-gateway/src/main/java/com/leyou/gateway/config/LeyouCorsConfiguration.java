5f95d9310169bf9cf46efa03574814f84073478a
ccw <913913144@qq.com>
2020-02-28 10:33:44 +0800

    2次提交

---
+++ leyou/leyou-gateway/src/main/java/com/leyou/gateway/config/LeyouCorsConfiguration.java
@@ -0,0 +1,37 @@
+package com.leyou.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@Configuration
+public class LeyouCorsConfiguration {
+    @Bean
+    public CorsFilter corsFilter(){
+        //1.添加CORS配置信息
+        CorsConfiguration config =new CorsConfiguration();
+        //允许的域，不要写*否则cookie就无法使用了
+        config.addAllowedOrigin("http://manage.leyou.com");
+        config.addAllowedOrigin("http://www.leyou.com");//搜索跨域问题
+        //是否发送Cookie信息
+        config.setAllowCredentials(true);
+        //允许的请求方式
+//        config.addAllowedMethod("OPTION");
+//        config.addAllowedMethod("HEAD");
+//        config.addAllowedMethod("GET");
+//        config.addAllowedMethod("PUT");
+//        config.addAllowedMethod("POST");
+//        config.addAllowedMethod("DELETE");
+//        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("*");
+        //允许的头信息
+        config.addAllowedHeader("*");
+        //2.cors配置源对象。添加映射路径。拦截一切请求
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**",config);
+        //3.返回新的corsFilter
+        return new CorsFilter(configSource);
+    }
+}
---
+++ leyou/leyou-search/src/main/java/com/leyou/search/service/SearchService.java
@@ -0,0 +1,246 @@
+package com.leyou.search.service;
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.pojo.*;
+import com.leyou.search.client.BrandClient;
+import com.leyou.search.client.CategoryClient;
+import com.leyou.search.client.GoodsClient;
+import com.leyou.search.client.SpecificationClient;
+import com.leyou.search.pojo.Goods;
+import com.leyou.search.pojo.SearchRequest;
+import com.leyou.search.pojo.SearchResult;
+import com.leyou.search.repository.GoodsRepository;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.LongTerms;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.FetchSourceFilter;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class SearchService {
+    @Autowired
+    private CategoryClient categoryClient;
+    @Autowired
+    private BrandClient brandClient;
+    @Autowired
+    private GoodsClient goodsClient;
+    @Autowired
+    private SpecificationClient specificationClient;
+    @Autowired
+    private GoodsRepository goodsRepository;
+    //json工具
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    /**
+     * 导入数据，即查询数据，把查询到的Spu转变为Goods来保存
+     * @param spu
+     * @return
+     * @throws IOException
+     */
+    public Goods buildGoods(Spu spu) throws IOException {
+        Goods goods = new Goods();
+        //根据分类的id查询分类名称
+        List<String> names = this.categoryClient.queryNameByIds(Arrays.asList(spu.getCid1(),spu.getCid2(),spu.getCid3()));
+        //根据品牌id查询品牌
+        Brand brand = this.brandClient.queryBrandById(spu.getBrandId());
+        //根据spuId查询所有的sku
+        List<Sku> skus=this.goodsClient.querySkuBySpuId(spu.getId());
+        //初始化一个价格集合，收集所有sku的价格
+        List<Long> prices =new ArrayList<>();
+        //收集sku必要字段信息
+        List<Map<String,Object>> skuMapList = new ArrayList<>();
+        skus.forEach(sku -> {
+            prices.add(sku.getPrice());
+            Map<String,Object> map = new HashMap<>();
+            map.put("id",sku.getId());
+            map.put("title",sku.getTitle());
+            map.put("price",sku.getPrice());
+            //获取sku中的图片，
+            //数据库中的图片可能是多张的，以" , " 分隔，所以 以逗号来切割返回图片数组，获取第一张图片
+            map.put("image", StringUtils.isBlank(sku.getImages()) ? "" : StringUtils.split(sku.getImages(),",")[0]);
+            skuMapList.add(map);
+        });
+        //根据spu中cid3查询出所有的搜索规格参数
+        List<SpecParam> params = this.specificationClient.queryParams(null, spu.getCid3(), null, true);
+
+        //根据spuId查询spuDetail
+        SpuDetail spuDetail=this.goodsClient.querySpuDetailById(spu.getId());
+        //把通用的规格参数值，进行反序列化,    TypeReference是一个接口
+        Map<String,Object> genericSpecMap =MAPPER.readValue(spuDetail.getGenericSpec(),
+                new TypeReference<Map<String,Object>>(){});
+        //把特殊的规格参数值，进行反序列化
+        Map<String,List<Object>> specialSpecMap =MAPPER.readValue(spuDetail.getSpecialSpec(),
+                new TypeReference<Map<String,List<Object>>>(){});
+
+        Map<String,Object> specs = new HashMap<>();
+        params.forEach(param ->{
+            //判断规格参数的类型，是否是通用的规格参数
+            if (param.getGeneric()){
+                //如果是通用类型的参数，从genericSpecMap获取规格参数值
+                String value = genericSpecMap.get(param.getId().toString()).toString();
+                //判断是否是数值类型，则返回一个区间
+                if (param.getNumeric()){
+                    value = chooseSegment(value,param);
+                }
+                specs.put(param.getName(),value);
+            }else {
+                //如果是特殊的规格参数，从specialSpecMap中获取值
+                List<Object> value = specialSpecMap.get(param.getId().toString());
+                specs.put(param.getName(),value);
+            }
+
+        });
+
+
+        goods.setId(spu.getId());
+        goods.setCid1(spu.getCid1());
+        goods.setCid2(spu.getCid2());
+        goods.setCid3(spu.getCid3());
+        goods.setBrandId(spu.getBrandId());
+        goods.setCreateTime(spu.getCreateTime());
+        goods.setSubTitle(spu.getSubTitle());
+        //拼接all字段，需要分类名称以及品牌名称,空格用于区分
+        goods.setAll(spu.getTitle() + " " + StringUtils.join(names," ") + " " + brand.getName());
+        //获取spu下所有sku的价格
+        goods.setPrice(prices);
+        //获取spu下的所有sku，并转换成json字符串
+        goods.setSkus(MAPPER.writeValueAsString(skuMapList));
+        //获取所有查询的规格参数{name:value}
+        goods.setSpecs(specs);
+        return goods;
+
+
+
+    }
+
+    //判断数值区间方法
+    private String chooseSegment(String value, SpecParam p) {
+        double val = NumberUtils.toDouble(value);
+        String result = "其它";
+        // 保存数值段，','区分 分区，"-"分组
+        //0-1.0,   1.0-1.5,  1.5-2.0  2.5...
+        for (String segment : p.getSegments().split(",")) {
+            String[] segs = segment.split("-");
+            // 获取数值范围
+            double begin = NumberUtils.toDouble(segs[0]);
+            double end = Double.MAX_VALUE;//防止第二个为空
+            if(segs.length == 2){
+                //如果有第二个，则取第二个
+                end = NumberUtils.toDouble(segs[1]);
+            }
+            // 判断是否在范围内
+            if(val >= begin && val < end){
+                if(segs.length == 1){
+                    result = segs[0] + p.getUnit() + "以上";//末位 2.5以上
+                }else if(begin == 0){
+                    result = segs[1] + p.getUnit() + "以下";//首位 0，取1以下
+                }else{
+                    result = segment + p.getUnit();
+                }
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 根据关键字搜索
+     * @param request
+     * @return
+     */
+    public SearchResult search(SearchRequest request) {
+
+        // 判断是否有搜索条件，如果没有，直接返回null。不允许搜索全部商品
+        if (StringUtils.isBlank(request.getKey())){
+            return null;
+        }
+        //自定义查询构建器
+        NativeSearchQueryBuilder queryBuilder =new NativeSearchQueryBuilder();
+        //添加查询条件
+        queryBuilder.withQuery(QueryBuilders.matchQuery("all",request.getKey()).operator(Operator.AND));
+        //添加分页，分页页码从0开始
+        queryBuilder.withPageable(PageRequest.of(request.getPage()-1,request.getSize()));
+        //添加结果集过滤
+        queryBuilder.withSourceFilter(new FetchSourceFilter(new String[]{"id","skus","subTitle"},null));
+
+        //添加分类和品牌的聚合
+        String categoryAggName ="categories";
+        String brandAggName ="brands";
+        queryBuilder.addAggregation(AggregationBuilders.terms(categoryAggName).field("cid3"));
+        queryBuilder.addAggregation(AggregationBuilders.terms(brandAggName).field("brandId"));
+
+        //执行查询，获取结果集
+        AggregatedPage<Goods> goodsPage = (AggregatedPage<Goods>)this.goodsRepository.search(queryBuilder.build());
+        //获取聚合结果并解析
+        List<Map<String,Object>> categories = getCategoryAggResult(goodsPage.getAggregation(categoryAggName));
+        List<Brand> brands = getBrandAggResult(goodsPage.getAggregation(brandAggName));
+        //封装结果返回
+        return new SearchResult(goodsPage.getTotalElements(),goodsPage.getTotalPages(),goodsPage.getContent(),categories,brands);
+    }
+
+    /**
+     * 解析品牌的聚合结果集
+     * @param aggregation
+     * @return
+     */
+    private List<Brand> getBrandAggResult(Aggregation aggregation) {
+        LongTerms terms = (LongTerms) aggregation;
+
+        //获取聚合中的桶
+
+        return terms.getBuckets().stream().map(bucket -> {
+
+           return this.brandClient.queryBrandById(bucket.getKeyAsNumber().longValue());
+        }).collect(Collectors.toList());
+
+ /*       // 获取所有的品牌id桶
+        List<LongTerms.Bucket> buckets = terms.getBuckets();
+        // 定义一个品牌集合，搜集所有的品牌对象
+        List<Brand> brands = new ArrayList<>();
+        // 解析所有的id桶，查询品牌
+        buckets.forEach(bucket -> {
+            Brand brand = this.brandClient.queryBrandById(bucket.getKeyAsNumber().longValue());
+            brands.add(brand);
+        });
+        return brands;*/
+    }
+
+    /**
+     * 解析分类的聚合结果集
+     * @param aggregation
+     * @return
+     */
+    private List<Map<String, Object>> getCategoryAggResult(Aggregation aggregation) {
+        LongTerms terms = (LongTerms) aggregation;
+        //获取桶的集合，转换成List<map<String,Object>>
+        return terms.getBuckets().stream().map(bucket -> {
+            //初始化map
+            Map<String,Object> map =new HashMap<>();
+            //获取分类id（key）
+            Long id =bucket.getKeyAsNumber().longValue();
+            //根据分类id查询分类名称
+            List<String> names =this.categoryClient.queryNameByIds(Arrays.asList(id));
+            map.put("id",id);
+            map.put("name",names.get(0));
+            return map;
+        }).collect(Collectors.toList());
+
+    }
+}
